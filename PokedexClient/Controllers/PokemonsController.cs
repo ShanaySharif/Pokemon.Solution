c60cb0726ad40e1b57a9807db0d8cbeadc9c3dc4
Austin Papritz <austin@papritz.dev>
2023-08-23 10:16:46 -0700

    removed unused usings

--- PokedexClient/Controllers/PokemonsController.cs
+++ PokedexClient/Controllers/PokemonsController.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using PokedexClient.Models;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 
--- PokedexClient/Models/ApplicationUser.cs
+++ PokedexClient/Models/ApplicationUser.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration.UserSecrets;
 
 
 namespace PokedexClient.Models;
