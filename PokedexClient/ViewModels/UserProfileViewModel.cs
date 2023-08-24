d12ad9ca30f448cda026829f2ed7bc8dd085fb84
Austin Papritz <austin@papritz.dev>
2023-08-24 10:21:18 -0700

    UserPRofileViewModel has UserName field

--- PokedexClient/ViewModels/UserProfileViewModel.cs
+++ PokedexClient/ViewModels/UserProfileViewModel.cs
@@ -5,6 +5,7 @@ public class UserProfileViewModel
 {
     public string Id { get; set; }
     public string Email { get; set; }
+    public string UserName { get; set; }
     public List<PokemonUser> PokemonUsers { get; set; }
     public virtual List<Pokemon> Pokemons { get; set; }
 }
